d124efeb640ada97f288cbf29f72b773a86ccc38
urishishi <atsuhito.yamaoka@datumstudio.jp>
2025-07-02 18:18:57 +0900

    parser

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-import logging
-import logging.handlers
-# from email import message
-import configparser
-import smtplib
-from email.mime import text
-from email.mime import multipart
+from optparse import OptionParser
 
 
-config = configparser.ConfigParser()
-config.read("./config.ini")
-# config_ini = configparser.ConfigParser()
-# config_ini.read("config.ini", encoding="utf-8")
+def main():
+    usage = "usage:%prog [options] arg1 arg2"
+    parser = OptionParser(usage)
+    parser.add_option(
+        "-f", "--file", action="store", type="string", dest="filename", help="File name"
+    )
+    options, args = parser.parse_args()
+    print(options)
+    print(args)
 
-smtp_host = "smtp.gmail.com"
-smtp_port = 587
-from_email = "atsuhito.yamaoka@datumstudio.jp"
-to_email = "atsuhito.yamaoka@supership.jp"
-user = config["user"]["user"]
-password = config["user"]["password"]
 
-# msg = message.EmailMessage()
-msg = multipart.MIMEMultipart()
-# msg.set_content("test email")
-msg["Subject"] = "test email subject"
-msg["From"] = from_email
-msg["To"] = to_email
-msg.attach(text.MIMEText("test email", "plain"))
-
-with open("./main.py", "r") as f:
-    attachment = text.MIMEText(f.read(), "plain")
-    attachment.add_header("Content-Disposition", "attachment", filename="main.py")
-    msg.attach(attachment)
-
-server = smtplib.SMTP(smtp_host, smtp_port)
-server.ehlo()
-server.starttls()
-server.ehlo()
-server.login(user, password)
-server.send_message(msg)
-server.quit()
+main()
